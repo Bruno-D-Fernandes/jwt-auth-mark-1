98da47a53b989b1457b70820ec76483115a11d51
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-05-31 22:53:04 -0300

    first commit

---
+++ src/main/java/edu/jwt_auth_mark_1/JwtAuthMark1Application.java
@@ -0,0 +1,13 @@
+package edu.jwt_auth_mark_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthMark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthMark1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/jwt_auth_mark_1/JwtAuthMark1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jwt_auth_mark_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthMark1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
